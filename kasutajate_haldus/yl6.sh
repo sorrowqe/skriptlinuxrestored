3f2f8f6918c7aadc0992c7951664c5215735ebc1
Aleksandr Skobioale <aleksandr.skobioale@voco.ee>
2025-11-03 18:12:17 -0400

    Skript, mis loob kasutajad ainult root-õigustega ja genereerib neile juhuslikud paroolid.

---
+++ kasutajate_haldus/yl6.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+kaivitav_kasutaja=$(whoami)
+if [ $kaivitav_kasutaja = "root" ]; then
+	if [ $# -ne 1 ]; then
+		echo "Kasutusjuhend: $0 kasutajad"
+	else
+		kasutajad=$1
+		if [ -f $kasutajad -a -r $kasutajad ]; then
+			echo "Failid on korras"
+			for rida in $(paste -d: $kasutajad $paroolid)
+			do
+				kasutajanimi=$(echo "$rida")
+				sh yl1 $kasutajanimi
+				parool=$(pwgen -s 8 -1)
+				echo "$kasutajanimi:$parool" | chpasswd
+				echo "$kasutajanimi - $parool" >> loodud_kasutajad_paroolidega
+			done
+		else
+			echo "Probleem failiga $failinimi"
+		fi
+	fi
+else
+	echo "Sul ei ole õigust antud skripti käivitamiseks"
+fi
